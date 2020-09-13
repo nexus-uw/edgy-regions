e5a558f93acd9299bcf671b34a3e369c7d013f9a
nexus-uw <nexus-uw@users.noreply.github.com>
2020-09-13 20:38:48 +0000

    Initial commit

---
+++ src/EdgyRegions/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EdgyRegions
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new EdgyRegionsStack(app, "EdgyRegionsStack");
+            app.Synth();
+        }
+    }
+}
